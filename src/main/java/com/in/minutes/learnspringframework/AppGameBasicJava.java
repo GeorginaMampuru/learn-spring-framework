507c89dea5ffc7e2c1971af3aa61c4b521667369
Georgina <66384787+GeorginaMampuru@users.noreply.github.com>
2023-09-10 17:32:27 +0200

    PacMan Game method added

--- src/main/java/com/in/minutes/learnspringframework/AppGameBasicJava.java
+++ src/main/java/com/in/minutes/learnspringframework/AppGameBasicJava.java
@@ -8,9 +8,9 @@ public class AppGameBasicJava {
 
         public static void main(String[] args) {
 
-            var game = new MarioGame();
+//            var game = new MarioGame();
 //           var game = new SuperContraGame();
-
+            var game = new PacMan();
             var gameRunner = new GameRunner(game);
             gameRunner.run();
         }
---
+++ src/main/java/com/in/minutes/learnspringframework/PacMan.java
@@ -0,0 +1,21 @@
+package com.in.minutes.learnspringframework;
+
+public class PacMan implements  GamingConsole{
+
+    public void up() {
+        System.out.println("up more" );
+    }
+
+    public void down() {
+        System.out.println("sit down");
+
+    }
+    public void left() {
+        System.out.println("Go Back");
+
+    }
+    public void right() {
+        System.out.println("shoot a bullet");
+
+    }
+}
