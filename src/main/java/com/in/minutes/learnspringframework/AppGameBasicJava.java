065b31b232f94e5f7fd8ace7016529da0d662d4e
Georgina <66384787+GeorginaMampuru@users.noreply.github.com>
2023-09-10 17:36:24 +0200

    loose coupling in java using the interfaces gaming console was added

--- src/main/java/com/in/minutes/learnspringframework/AppGameBasicJava.java
+++ src/main/java/com/in/minutes/learnspringframework/AppGameBasicJava.java
@@ -9,7 +9,7 @@ public class AppGameBasicJava {
         public static void main(String[] args) {
 
 //            var game = new MarioGame();
-//           var game = new SuperContraGame();
+           var game = new SuperContraGame();
             var game = new PacMan();
             var gameRunner = new GameRunner(game);
             gameRunner.run();
